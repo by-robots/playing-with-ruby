104e666e2ff566eed26718140a7e8d564ad75507
Dan <skynet@by-robots.com>
2019-06-12 13:55:40 +0100

    Code blocks as interators.

--- blocks.rb
+++ blocks.rb
@@ -1,8 +1,2 @@
-def call_block
-  puts "hello"
-  yield('Bruce')
-  yield('Wayne')
-  puts "How are you?"
-end
-
-call_block {|name| puts name }
+animals = %w{ dog cat badger racoon }
+animals.each { |animal| puts animal }
