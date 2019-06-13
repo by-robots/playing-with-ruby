e3f8216106baad679dd4d17f02148e99d0af03fc
Dan <skynet@by-robots.com>
2019-06-13 13:40:48 +0100

    Remove all of the test code.

--- book_in_stock.rb
+++ book_in_stock.rb
@@ -24,25 +24,3 @@ class BookInStock
     @price = pence / 100.0
   end
 end
-
-# Create some instances and see what's inside.
-b1 = BookInStock.new('isbn1', 3)
-puts "Book 1's ISBN is #{b1.isbn}."
-puts "Book 1's price is #{b1.price}."
-puts "Book 1's price in pence is #{b1.price_in_pence}."
-
-b2 = BookInStock.new('isbn2', 3.13)
-puts "Book 2's ISBN is #{b2.isbn}."
-puts "Book 2's price is #{b2.price}."
-
-# Reset Book 2's price by supplying a number of pence.
-b2.price_in_pence = 450
-puts "Book 2's new price is #{b2.price}."
-
-b3 = BookInStock.new('isbn3', '5.67')
-puts "Book 3's ISBN is #{b3.isbn}."
-puts "Book 3's price is #{b3.price}."
-
-# Apply a discount to Book 3 to test out writable attributes.
-b3.price = b3.price * 0.75
-puts "Book 3's discounted price is #{b3.price}, which is #{b3.price_in_pence} pence."
