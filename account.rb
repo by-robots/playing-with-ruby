1d1f8342bfe8167b7d74e3372db8bf5d5ea4acea
Dan <skynet@by-robots.com>
2019-06-14 13:21:58 +0100

    Corrections.

--- account.rb
+++ account.rb
@@ -1,6 +1,7 @@
 class Account
   attr_accessor :balance
-  attr_reader   :cleared_balance
+  attr_reader   :cleared_balance # Define an accessor for cleared_balance...
+  protected     :cleared_balance # ...but only have it available to other Account instances
 
   def initialize(balance)
     @balance = balance
@@ -9,6 +10,6 @@ class Account
   protected
 
   def greater_balance_than?(other)
-    @cleared_balance > other.cleared_balanace
+    @cleared_balance > other.cleared_balance
   end
 end
