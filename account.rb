0f0007fa34ab06d1d5692c19674fd5765dba8eb3
Dan <skynet@by-robots.com>
2019-06-14 13:19:01 +0100

    Fiddle with protected attributes.

--- account.rb
+++ account.rb
@@ -1,7 +1,14 @@
 class Account
   attr_accessor :balance
+  attr_reader   :cleared_balance
 
   def initialize(balance)
     @balance = balance
+  end
+
+  protected
+
+  def greater_balance_than?(other)
+    @cleared_balanace > other.cleared_balanace
   end
 end
