7e60807e9f8ea3b3ed7851039c6fd0eb56082e7a
Dan Gates <skynet@by-robots.com>
2019-06-20 11:19:02 +0100

    Split up code to demonstrate how filter methods return enumerator objects.

--- lazy_enumerators.rb
+++ lazy_enumerators.rb
@@ -5,10 +5,18 @@ def Integer.all
   end.lazy
 end
 
-# If the all method didn't use a lazy enumerator this would loop forever because
-# the select call would try to read all values from the enumerator in all, so it
-# would never return. By making rhe numerator itself lazy, select will only use
-# the values that it needs.
-p Integer.all
-         .select { |i| (i % 3).zero? }
-         .first(10)
+# Is the given value a palindrome?
+def palindrome?(value)
+  value = value.to_s
+  value == value.reverse
+end
+
+# If the all method didn't use a lazy enumerator these would loop forever
+# because the select call would try to read all values from the enumerator in
+# all, so it would never return. By making the numerator itself lazy, select
+# will only use the values that it needs.
+multiple_of_three = Integer.all.select { |i| (i % 3).zero? }
+p multiple_of_three.first(10)
+
+m3_palindromes = multiple_of_three.select { |i| palindrome?(i) }
+p m3_palindromes.first(10)
