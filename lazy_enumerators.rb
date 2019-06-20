5e235a5274d03ec4982871480650ab1a9c514a0c
Dan Gates <skynet@by-robots.com>
2019-06-20 11:23:18 +0100

    And, just for fun, re-write using lambda functions.

--- lazy_enumerators.rb
+++ lazy_enumerators.rb
@@ -5,18 +5,14 @@ def Integer.all
   end.lazy
 end
 
-# Is the given value a palindrome?
-def palindrome?(value)
-  value = value.to_s
-  value == value.reverse
-end
-
 # If the all method didn't use a lazy enumerator these would loop forever
 # because the select call would try to read all values from the enumerator in
 # all, so it would never return. By making the numerator itself lazy, select
 # will only use the values that it needs.
-multiple_of_three = Integer.all.select { |i| (i % 3).zero? }
-p multiple_of_three.first(10)
+multiple_of_three = ->(n) { (n % 3).zero? }
+palindrome = ->(n) { n.to_s == n.to_s.reverse }
 
-m3_palindromes = multiple_of_three.select { |i| palindrome?(i) }
-p m3_palindromes.first(10)
+p Integer.all
+  .select(&multiple_of_three)
+  .select(&palindrome)
+         .first(100)
