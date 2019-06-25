3626a883fe69797691beef199950b8348843cc91
Dan <skynet@by-robots.com>
2019-06-25 13:17:56 +0100

    A basic inheritance example.

---
+++ basic_inheritance.rb
@@ -0,0 +1,16 @@
+# Define a parent class
+class Parent
+  def say_hello
+    puts "Hello from #{self}"
+  end
+end
+
+p = Parent.new
+p.say_hello # Hello from #<Parent:0x00007fc24584fa58>
+
+# Create a subclass of the parent
+class Child < Parent
+end
+
+c = Child.new
+c.say_hello # Hello from #<Child:0x00007fc24584f850>
